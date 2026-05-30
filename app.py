73df1df2d4b0f0b1e166449bc85c7e46d01349e2
Itamar <itama@example.com>
2026-05-30 13:17:36 +0300

    /

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello():
     return "Hello from Lab Test APP"
 
+@app.route("/Chat")
+def hello():
+    return "Wellcome to the chat"
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
